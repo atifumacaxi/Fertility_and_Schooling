e973511b8c45fab967ddb2f796ada2d0e502b9f2
lucas silvestre <atifumacaxi@gmail.com>
2023-06-09 16:45:28 -0300

    Preprocessing and model classes

---
+++ interface/main_local.py
@@ -0,0 +1,4 @@
+from fertility_and_schooling import *
+from taxifare.ml_logic.data import clean_data
+from taxifare.ml_logic.preprocessing import preprocessing_features
+from taxifare.ml_logic.model import compile_model, initialize_model, train_model
