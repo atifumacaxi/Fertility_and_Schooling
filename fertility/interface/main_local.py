6c87380bd0e1bf36d5c105ab0341f0a155124ec8
lucas silvestre <atifumacaxi@gmail.com>
2023-06-12 12:00:16 -0300

    Packaging OK

--- fertility/interface/main_local.py
+++ fertility/interface/main_local.py
@@ -1,6 +1,6 @@
-from Fertility_and_Schooling.ml_logic.data import clean_data
-from Fertility_and_Schooling.ml_logic.preprocessing import preprocessing_features
-from Fertility_and_Schooling.ml_logic.model import create_X_y, compile_model, initialize_model, train_model
+#from fertility.ml_logic import clean_data
+from fertility.ml_logic.preprocessing import preprocessing_features
+from fertility.ml_logic.model import create_X_y, compile_model, initialize_model, train_model
 from sklearn.model_selection import train_test_split
 
 import pandas as pd
@@ -9,9 +9,9 @@ import numpy as np
 def preprocess_and_train():
 
     #Loading schooling dataset
-    df_schooling = pd.read_csv('mean-years-of-schooling-long-run.csv', sep=';')
+    df_schooling = pd.read_csv('fertility/data/mean-years-of-schooling-long-run.csv', sep=';')
     #Loading fertility dataset
-    df_fertility = pd.read_csv('../data/fertility_rate.csv')
+    df_fertility = pd.read_csv('fertility/data/fertility_rate.csv')
 
     df = preprocessing_features(df_schooling, df_fertility)
 
@@ -21,6 +21,7 @@ def preprocess_and_train():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, shuffle=False)
 
     # Train a model on the training set, using `model.py`
+    model = None
     model = initialize_model(model)
     model = compile_model(model)
     model, history = train_model(model, X_train, y_train)
